7fe908233754afa14fd9601f2e648b3a1fe811d4
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-11-11 16:58:59 +0530

    model class Employee

---
+++ jpa-one-to-one-example-spring-boot/src/main/java/com/zeeshan/model/Employee.java
@@ -0,0 +1,39 @@
+package com.zeeshan.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee implements Serializable {
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String employeeName;
+	private String employeeCode;
+	private String designation;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "addressId")
+	private Address address;
+	
+}
