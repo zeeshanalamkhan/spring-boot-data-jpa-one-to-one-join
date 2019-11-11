0c81de921303c6f853c53dbf2855970c888cfe11
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-11-11 16:57:32 +0530

    spring boot main method class

---
+++ jpa-one-to-one-example-spring-boot/src/main/java/com/zeeshan/JpaOneToOneExampleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.zeeshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToOneExampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOneToOneExampleSpringBootApplication.class, args);
+	}
+
+}
